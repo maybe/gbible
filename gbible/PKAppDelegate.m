a84c7b19ec832f9865e4ba8e484d2e4acaf1f268
Kerri Shotts <kerrishotts@gmail.com>
2013-01-12 00:13:09 -0600

    attempt to restore brightness when backgrounded; doesn't seem to work (looks like a bug in Apple's code)

--- gbible/PKAppDelegate.m
+++ gbible/PKAppDelegate.m
@@ -58,6 +58,7 @@
 @synthesize rootViewController;
 @synthesize segmentController;
 @synthesize segmentedControl;
+@synthesize brightness;
 
     static id _instance;
     
@@ -253,6 +254,10 @@
     [self.window makeKeyAndVisible];
 
     [TestFlight passCheckpoint:@"APPLICATION_START"];
+  
+    // since we alter the brightness, get the value now
+    brightness = [[UIScreen mainScreen] brightness];
+    
     return YES;
 }
 
@@ -265,6 +270,8 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // and restore the brightness
+    [[UIScreen mainScreen] setBrightness:brightness];
 }
 
 /**
@@ -276,7 +283,10 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
+
+    // and restore the brightness
+    [[UIScreen mainScreen] setBrightness:brightness];
+  
     // before going, get the top verse
     ZUUIRevealController  *rc = (ZUUIRevealController *)self.rootViewController;
     PKRootViewController *rvc = (PKRootViewController *)rc.frontViewController;
@@ -292,6 +302,7 @@
   
     // save our settings
     [[PKSettings instance ]saveSettings];
+  
 }
 
 /**
@@ -302,6 +313,7 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    brightness = [[UIScreen mainScreen] brightness];
 }
 
 /**
@@ -323,6 +335,8 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // and restore the brightness
+    [[UIScreen mainScreen] setBrightness:brightness];
     [[PKSettings instance ]saveSettings];
 }
 
